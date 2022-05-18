3296a3494c6011152e7135989de65cd0b77d7e62
Medis Redzic <58595200+medisredzic@users.noreply.github.com>
2022-05-18 21:54:38 +0200

    Final version, outputs img_arr(image after removing offset and spacing), known_array(known pixels are 1 and unknown 0), target_array(removed pixels in img_arr)

--- ex4.py
+++ ex4.py
@@ -1,3 +1,9 @@
+"""
+Author: Medis Redzic
+Matr.Nr.: K11934616
+Exercise: Exercise 4
+"""
+
 import numpy as np
 from PIL import Image
 
@@ -24,32 +30,38 @@ def ex4(image_array: np.ndarray, offset: tuple, spacing: tuple):
     if not 2 <= spacing[0] <= 8 or not 2 <= spacing[1] <= 8:
         raise ValueError('Spacing can not be smaller than 2 or larger than 8')
 
+    n_axis, m_axis = image_array.shape[0] - offset[0], image_array.shape[1] - offset[1]
+    n_axis_s, m_axis_s = n_axis / spacing[0], m_axis / spacing[1]
+
+    if (n_axis_s * m_axis_s) < 144:
+        raise ValueError(f'Remaining pixels can not be under 144! Remaining pixels: {n_axis * m_axis_s}')
+
     img_arr = np.zeros_like(image_array)
+    img_arr = np.transpose(img_arr, (2, 0, 1))
 
-    for m in range(0, img_arr.shape[0]):
-        for n in range(0, img_arr.shape[1], spacing[1]):
-            img_arr[m, n, :] = image_array[m, n, :]
+    for m in range(offset[1], img_arr.shape[1], spacing[1]):
+        for n in range(offset[0], img_arr.shape[2], spacing[0]):
+            img_arr[:, m, n] = image_array[m, n, :]
 
-    for m in range(0, img_arr.shape[1]):
-        for n in range(0, img_arr.shape[0], spacing[0]):
-            img_arr[n, m, :] = 0
+    target_array = np.zeros_like(img_arr)
+    known_array = np.zeros_like(img_arr)
 
-    img_arr[:, :offset[0], :] = 0
-    img_arr[:offset[1], :, :] = 0
+    known_array[img_arr > 0] = 1
 
-    remaining_pixels = (np.sum(np.all(img_arr > 0, axis=2))) // 3
+    target_array = img_arr[known_array == 0]
+    target_array = target_array.flatten()
 
-    if remaining_pixels < 144:
-        raise ValueError(f'Remaining pixels can not be under 144! Remaining pixels: {remaining_pixels}')
-
-    return img_arr
+    return img_arr, known_array, target_array
 
 
 if __name__ == '__main__':
-    img_path = 'img.jpg'
+    img_path = '336.jpg'
     open_img = Image.open(img_path)
     image_array = np.asarray(open_img)
-    arr = ex4(image_array, (2, 1), (2, 3))
+    arr, _, _ = ex4(image_array, (0, 0), (8, 8))
 
+    #img_faa = Image.fromarray(kw)
+    #img_faa.save('img_kw.jpg')
+    arr = np.transpose(arr, (2, 1, 0))
     img_fa = Image.fromarray(arr)
     img_fa.save('img_after.jpg')
